fcd69608b3a88520db1db2d04a4dcf7e9173209c
Lean Meza <leanmeza@debian-BULLSEYE-live-builder-AMD64>
2023-03-20 10:02:10 -0300

    inicio del proyecto completo con vue3

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import { createApp } from "vue";
+
+import app from "../src/App.vue";
+
+createApp(app).mount("#app");
---
+++ resources/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>LARAVEL 10 + VUE 3</h1>
+	<h1>LARAVEL 10 + VUE 3</h1>
+	<h1>FIRST COMMIT</h1>
+</template>
+
+<script setup>
+</script>
+
+
